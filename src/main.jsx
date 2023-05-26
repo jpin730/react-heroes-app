1e209125347a70f0b47dba6f35f6dd7e3cfb1ec9
Jaime Pineda <jpin730@pm.me>
2023-05-25 22:45:54 -0600

    fix: :bug: change browser router to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { HeroesApp } from "./HeroesApp";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={import.meta.env.PROD ? "/react-heroes-app" : "/"}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <HeroesApp />
-    </React.StrictMode>
-  </BrowserRouter>
+    </HashRouter>
+  </React.StrictMode>
 );
